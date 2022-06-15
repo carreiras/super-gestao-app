5de4dd92533f185e0ddc8b29495ca85dfe1b6703
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-06-14 23:40:21 -0300

    Blade - Escapando a tag de impressão do Blade

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -30,8 +30,6 @@ class FornecedorController extends Controller
             ],
         ];
 
-        $fornecedores = [];
-
         return view('app.fornecedor.index', compact('fornecedores'));
     }
 }
