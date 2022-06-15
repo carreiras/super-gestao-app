ed79df4fc48363bfb8acb0bd7a73977fe2913358
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-06-14 23:12:14 -0300

    Blade - @for

--- resources/views/app/fornecedor/index.blade.php
+++ resources/views/app/fornecedor/index.blade.php
@@ -8,27 +8,14 @@
 @endphp
 
 @isset($fornecedores)
-    Fornecedor: {{ $fornecedores[0]['nome'] }}
-    <br />
-    Status: {{ $fornecedores[0]['status'] }}
-    <br />
-    CNPJ: {{ $fornecedores[1]['cnpj'] ?? '' }}
-    <br />
-    Telefone: {{ $fornecedores[1]['ddd'] ?? '' }} {{ $fornecedores[1]['telefone'] ?? '' }}
-    @switch($fornecedores[1]['ddd'])
-        @case('11')
-            São Paulo - SP
-        @break
-
-        @case('85')
-            Fortaleza - CE
-        @break
-
-        @case('32')
-            Juiz de Fora - MG
-        @break
-
-        @default
-            Estado não identificado
-    @endswitch
+    @for ($i = 0; isset($fornecedores[$i]); $i++)
+        Fornecedor: {{ $fornecedores[0]['nome'] }}
+        <br />
+        Status: {{ $fornecedores[0]['status'] }}
+        <br />
+        CNPJ: {{ $fornecedores[1]['cnpj'] ?? '' }}
+        <br />
+        Telefone: {{ $fornecedores[1]['ddd'] ?? '' }} {{ $fornecedores[1]['telefone'] ?? '' }}
+        <hr>
+    @endfor
 @endisset
